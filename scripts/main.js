14efac1f9912494f4854add38af25868fd8aa673
ZaibLComrade <kmjahanzaib@gmail.com>
2023-08-17 20:51:13 +0600

    Font-Size input functional

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,35 @@
-function format(input, type) {
+function format(input, value) {
 	const output = document.getElementById("text-input");
 	if(input.type === "submit") {
 		input.classList.toggle("bg-cyan-400");
 		input.classList.toggle("hover:bg-transparent");
+		switch(value) {
+			case "bold":
+				output.classList.toggle("font-bold");	
+				break;
+			case "italic":
+				output.classList.toggle("italic");
+				break;
+			case "underline": 
+				output.classList.toggle("underline");
+				break;
+			case "left":
+				output.classList.toggle("text-left");
+				break;	
+			case "center":
+				output.classList.toggle("text-center");
+				break;
+			case "right":
+				output.classList.toggle("text-right");
+				break;
+			case "justify":
+				output.classList.toggle("text-jusitfy");
+				break;
+		}
 	}
 
-	switch(type) {
-		case "bold":
-			output.classList.toggle("font-bold");	
-			break;
-		case "italic":
-			output.classList.toggle("italic");
-			break;
-		case "underline": 
-			output.classList.toggle("underline");
-			break;
-		case "left":
-			output.classList.toggle("text-left");
-			break;	
-		case "center":
-			output.classList.toggle("text-center");
-			break;
-		case "right":
-			output.classList.toggle("text-right");
-			break;
-		case "justify":
-			output.classList.toggle("text-jusitfy");
-			break;
+	if(input.type === "number") {
+		output.style.fontSize = `${value}px`;
 	}
 }
 
@@ -58,13 +61,31 @@ underlineBtn.addEventListener("click", () => {
 	format(underlineBtn, "underline");
 }) 
 
-const alignBtns = ["align-left-btn", "align-center-btn", "align-right-btn", "align-justify-btn"];
-const types = ["left", "center", "right", "justify"];
+// const alignBtns = ["align-left-btn", "align-center-btn", "align-right-btn", "align-justify-btn"];
+// const types = ["left", "center", "right", "justify"];
 
-for(let i = 0; i < alignBtns.length; i++) {
-	const currBtn = document.getElementById(alignBtns[i]);
+const types = {
+	"align-left-btn": "left",
+	"align-center-btn": "center",
+	"align-right-btn": "right",
+	"align-justify-btn": "justify"
+}
+
+for(let i = 0; i < Object.keys(types).length; i++) {
+	const currBtn = document.getElementById(Object.keys(types)[i]);
 	currBtn.addEventListener("click", () => {
-		format(currBtn, types[i]);
-		disableExcept(alignBtns, types[i]);
+		format(currBtn, Object.values(types)[i]);
+		disableExcept(Object.keys(types), Object.values(types)[i]);
 	})
 }
+
+const numberInp = document.getElementById("number-input");
+numberInp.addEventListener("click", () => {
+	const value = parseInt(numberInp.value);
+	if(value >= 10) format(numberInp, value);
+})
+
+numberInp.addEventListener("keyup", () => {
+	const value = parseInt(numberInp.value);
+	if(value >= 10) format(numberInp, value);
+})
