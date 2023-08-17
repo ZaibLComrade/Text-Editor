d84a23874bfec798ce7c5a0e8b0b492e25fec492
ZaibLComrade <kmjahanzaib@gmail.com>
2023-08-17 15:09:51 +0600

    Added button functionality

--- scripts/main.js
+++ scripts/main.js
@@ -9,10 +9,26 @@ function format(input, type) {
 		case "bold":
 			output.classList.toggle("font-bold");	
 			break;
+		case "italic":
+			output.classList.toggle("italic");
+			break;
+		case "underline": 
+			output.classList.toggle("underline");
+			break;
 	}
 }
 
 const boldBtn = document.getElementById("bold-btn");
 boldBtn.addEventListener("click", () => {
 	format(boldBtn, "bold");
+})
+
+const italicBtn = document.getElementById("italic-btn");
+italicBtn.addEventListener("click", () => {
+	format(italicBtn, "italic");
+})
+
+const underlineBtn = document.getElementById("underline-btn");
+underlineBtn.addEventListener("click", () => {
+	format(underlineBtn, "underline");
 })
