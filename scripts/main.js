b2458e0c9f9c3ceb23515b3f1808d977205a0bc7
ZaibLComrade <kmjahanzaib@gmail.com>
2023-08-17 18:56:28 +0600

    Align Buttons Functional

--- scripts/main.js
+++ scripts/main.js
@@ -15,6 +15,31 @@ function format(input, type) {
 		case "underline": 
 			output.classList.toggle("underline");
 			break;
+		case "left":
+			output.classList.toggle("text-left");
+			break;	
+		case "center":
+			output.classList.toggle("text-center");
+			break;
+		case "right":
+			output.classList.toggle("text-right");
+			break;
+		case "justify":
+			output.classList.toggle("text-jusitfy");
+			break;
+	}
+}
+
+function disableExcept(btnsArr, alignType) {
+	defTypes = ["left", "center", "right", "justify"];
+	let output = document.getElementById("text-input");
+	for(let i = 0; i < defTypes.length; i++) {
+		if(alignType !== defTypes[i]) {
+			const btn = document.getElementById(btnsArr[i]);
+			output.classList.remove(`text-${defTypes[i]}`);
+			btn.classList.remove("bg-cyan-400");
+			btn.classList.remove("hover:bg-transparent");
+		}	
 	}
 }
 
@@ -31,4 +56,15 @@ italicBtn.addEventListener("click", () => {
 const underlineBtn = document.getElementById("underline-btn");
 underlineBtn.addEventListener("click", () => {
 	format(underlineBtn, "underline");
-})
+}) 
+
+const alignBtns = ["align-left-btn", "align-center-btn", "align-right-btn", "align-justify-btn"];
+const types = ["left", "center", "right", "justify"];
+
+for(let i = 0; i < alignBtns.length; i++) {
+	const currBtn = document.getElementById(alignBtns[i]);
+	currBtn.addEventListener("click", () => {
+		format(currBtn, types[i]);
+		disableExcept(alignBtns, types[i]);
+	})
+}
